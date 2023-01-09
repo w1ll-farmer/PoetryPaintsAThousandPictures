67c8c4d5bfcabc9ac5d72d781d74c6c206ab9122
w1ll-farmer <williamsfarmer@icloud.com>
2023-01-09 13:53:58 +0000

    basic server running

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const app = express()
+app.use(express.static('nodeJS'));
 
+app.get('/', function(req, resp){
+    resp.send("Hello World")
+})
+
+app.listen(8090)
