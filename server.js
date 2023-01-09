9c9f743e9ae3d381ae29ec554c454db9643c332a
w1ll-farmer <williamsfarmer@icloud.com>
2023-01-09 14:24:09 +0000

    console.log added

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express()
 app.use(express.static('nodeJS'));
 
 app.get('/', function(req, resp){
-    resp.send("Hello World")
+    resp.statusCode = 200
+    resp.setHeader('Content-Type', 'text/plain');
+    resp.end("Hello World")
+    
 })
 
 app.listen(8090)
+console.log('server running at http//127.0.0.1:8090/')
