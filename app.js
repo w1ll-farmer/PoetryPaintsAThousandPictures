5ca7fe175ed7e5ed0031a81e1854ab87e187cb55
w1ll-farmer <williamsfarmer@icloud.com>
2023-01-13 16:21:38 +0000

    Made images work

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('client'));
-app.get('/', function(req, resp){
-    
+app.get('/poems', function(req, resp){
+    resp.send('Poems innit')
 })
-
-
+function switchDiv(divId) {
+    var divs = document.getElementsByTagName("div");
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].style.display = "none";
+    }
+    document.getElementById(divId).style.display = "block";
+}
+//onclick=switchDiv('intro-page-div')
 
 module.exports = app;
---
+++ client/div-select.js
@@ -0,0 +1,13 @@
+function switchDiv(divClass) {
+    var divs = document.getElementsByTagName("div");
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].classList.add("hidden");
+    }
+    
+    var elements = document.getElementsByClassName(divClass);
+
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove("hidden");
+}
+
+}
