f0d1060516e12e0661a11b9111809c949c415240
w1ll-farmer <williamsfarmer@icloud.com>
2023-01-16 14:53:09 +0000

    successfully write to JSON file

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('client'));
-app.get('/poems', function(req, resp){
-    resp.send('Poems innit')
-})
-function switchDiv(divId) {
-    var divs = document.getElementsByTagName("div");
-    for (var i = 0; i < divs.length; i++) {
-        divs[i].style.display = "none";
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client')));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const fs = require('fs');
+
+app.post('/save-json', (req, res) => {
+  // Read the JSON data from the request body
+  const jsonData = req.body;
+  const data = {};
+  data[jsonData.poem] = jsonData.author;
+  //const jsonString = JSON.stringify(jsonData)
+  // Specify the destination file
+  const filePath = './poems.json';
+  fs.readFile(filePath, 'utf8', (err, fileData) =>{
+    if (err) {
+        console.error(err)
+        res.status(500).send('Error reading the file.');
+    } else {
+        let jsonFile = {};
+        if (fileData) {
+            jsonFile = JSON.parse(fileData);
+        }
+        Object.assign(jsonFile, data);
+        fs.writeFile(filePath, JSON.stringify(jsonFile), (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Error saving the file.')
+            } else {
+                res.status(200).send('File saved successfully.');
+            }
+        })
     }
-    document.getElementById(divId).style.display = "block";
-}
-//onclick=switchDiv('intro-page-div')
+  })
+  
+  });
+
+
+
 
 module.exports = app;
--- client/poem-read-write.js
+++ client/poem-read-write.js
@@ -1,13 +1,34 @@
-const endpontRoot = 'http://127.0.0.1:8090/';
-async function addPoems(){
-    const writeForm = document.getElementById('write-form');
-    writeForm.addEventListener('submit', async function (event){
-        event.preventDefault();
-
-        const data = new FormData(writeForm);
-/*JSON conversion from https://stackoverflow.com/questions/41431322/how-to-convert-formdata-html5-object-to-json */
-        const dataJSON = JSON.stringify(Object.fromEntries(data));
-
-        const response = await fetch()
-    })
-}
+async function saveData() {
+    // Get the poem and author from the form
+    const poem = document.querySelector("textarea[name='poem']").value;
+    const author = document.querySelector("input[name='author']").value;
+  
+    // Create a data object to hold the form data
+    const data = { poem, author };
+  
+    // Convert the data object to a JSON string
+    const jsonData = JSON.stringify(data);
+  
+    try {
+      // Use the fetch() function to send a request to the server to save the JSON data to a file
+      const response = await fetch('/save-json', {
+        method: 'POST',
+        body: jsonData,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const result = await response.json();
+      console.log('Success:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  
+  const form = document.querySelector("#write-form");
+  form.addEventListener("submit", x => {
+    x.preventDefault();
+    saveData();
+    document.getElementById("write-form").reset();
+  });
+  
